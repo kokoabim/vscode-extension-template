880ec0a35866716132d927ce5507f6ae1046593b
Spencer James <spencer.ws.james@me.com>
2025-03-17 21:54:20 -0700

    misc eslint stuff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,48 @@
+import js from "@eslint/js";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
+export default [
+    {
+        files: ["**/*.ts"],
+        ignores: ["**/test/**/*"],
 
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-    },
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
 
-    rules: {
-        "@typescript-eslint/explicit-function-return-type": "warn",
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-        "@typescript-eslint/no-unused-expressions": "warn",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "curly": [
-            "warn",
-            "multi-line"
-        ],
-        "eqeqeq": "warn",
-        "no-throw-literal": "warn",
-        "no-unreachable": "warn",
-        "no-unused-expressions": "warn",
-        "semi": "warn",
-    },
-}];
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2022,
+            sourceType: "module",
+        },
+
+        rules: {
+            ...js.configs.recommended.rules,
+            ...typescriptEslint.configs.recommended.rules,
+
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/explicit-function-return-type": "warn",
+            "@typescript-eslint/naming-convention": ["warn", {
+                selector: "import",
+                format: ["camelCase", "PascalCase"],
+            }],
+            "@typescript-eslint/no-unused-expressions": "warn",
+            "@typescript-eslint/no-unused-vars": "warn",
+
+            "curly": [
+                "warn",
+                "multi-line"
+            ],
+            "eqeqeq": "warn",
+            "no-throw-literal": "warn",
+            "no-unreachable": "warn",
+            "no-unused-expressions": "warn",
+            "semi": "warn",
+            "prefer-const": "warn",
+            "no-undef": "off",
+            "no-empty": "off",
+            "no-unused-vars": "off",
+        },
+    }
+];
